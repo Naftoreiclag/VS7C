639e8ed0262307fd0876111db12d49dcaa8d1867
Naftoreiclag <naftoreiclag@gmail.com>
2015-08-15 15:08:06 -0700

    Server stuff

---
+++ src_server/main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+#include "irrlicht.h"
+#include "SFML/System.hpp"
+#include "SFML/Network.hpp"
+
+int main() {
+    std::cout << "SK Server" << std::endl;
+
+    sf::UdpSocket serverSocket;
+
+    unsigned short port = 25566;
+
+    if(!serverSocket.bind(port)) {
+        // Explode
+    }
+
+    sf::Packet chatPacket;
+    sf::IpAddress sender;
+
+
+    bool runServer = true;
+    while(runServer) {
+        if(serverSocket.receive(chatPacket, sender, port) != sf::Socket::Done) {
+            // Explode
+        }
+
+        std::string message;
+        chatPacket >> message;
+
+        std::cout << sender << ":" << port << std::endl;
+        std::cout << "> " << message << std::endl;
+
+        if(message == "stop") {
+            runServer = false;
+        }
+    }
+}
